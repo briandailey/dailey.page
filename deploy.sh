8ce1cda745b90e89bb0661c42c002f8efd38cec5
Brian Dailey <brian@dailytechnology.net>
2023-03-22 15:32:59 -0500

    Various changes.

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,17 @@ rm -rf public && hugo
 
 function deploy () {
     bucket="s3://drafts.dailey.page"
+    url="http://drafts.dailey.page.s3-website.us-east-2.amazonaws.com"
 
     if [[ $1 == "prod" ]]; then
         bucket="s3://dailey.page/"
+        url="http://dailey.page.s3-website-us-east-1.amazonaws.com/"
     fi
 
     echo "Deploying to $bucket..."
     aws --profile dailey s3 sync public/. $bucket
-    echo "Done. 👍🏻"
+    echo "Done. Published to $url"
+    echo "👍🏻"
 }
 
 deploy $1
